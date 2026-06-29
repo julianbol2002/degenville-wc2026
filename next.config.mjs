bab62a10c40556ed6e496fe60e61ba6cc42f3d42
julianbol2002 <julianbolanos52@gmail.com>
2026-06-29 13:59:35 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "flagcdn.com",
       },
+      {
+        protocol: "https",
+        hostname: "axwayhelufjerkejbwtn.supabase.co",
+      },
     ],
   },
 };
